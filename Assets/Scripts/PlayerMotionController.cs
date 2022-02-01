a4d653ce58a51809c36b11b286dd46b5f06d8b1c
EleouetClement <eleouet.clement@outlook.fr>
2022-02-01 13:37:36 +0100

    adding inputs
    Using input Manager of unity to set inputs for a character

--- Assets/Scripts/PlayerMotionController.cs
+++ Assets/Scripts/PlayerMotionController.cs
@@ -7,13 +7,19 @@ public class PlayerMotionController : MonoBehaviour
 {
     [SerializeField] [Range(0, 20)] private float walkSpeed = 1f;
     [SerializeField] [Range(0, 200)] private float rotationSpeed = 1f;
-    [SerializeField] [Range(0, 10)] private float jumpForce = 1f;
+    [SerializeField] [Range(0, 10)] private float jumpMaxHeight = 1f;
     private Rigidbody char_body;
     private PlayerInput char_playerInput;
     private CharacterController char_characterControler;
     public Animator char_animator;
-    private float forwardDirection;
-    private float rightDirection;
+    public float gravity = -9.81f;
+
+    private Vector3 forwardDirection;
+    private Vector3 rightDirection;
+    private Vector2 inputAxis;
+    private Vector3 velocity;
+    
+    private bool isJumping = false;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -22,26 +28,48 @@ public class PlayerMotionController : MonoBehaviour
     }
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        forwardDirection = Input.GetAxis("Vertical");
-        rightDirection = Input.GetAxis("Horizontal");
+        //forwardDirection = Input.GetAxis("Vertical");
+        //rightDirection = Input.GetAxis("Horizontal");
 
     }
 
     private void FixedUpdate()
     {
         Move();
+        Jump();
         Rotate();
+        AddForce();
     }
 
+    /// <summary>
+    /// Handles moving inputs using InputSystem
+    /// </summary>
+    /// <param name="value"></param>
     void OnMove(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
+        //Debug.Log(value.Get<Vector2>());
+        inputAxis = value.Get<Vector2>();
+        
+    }
+
+    /// <summary>
+    /// Handles Jump inputs
+    /// </summary>
+    /// <param name="value"></param>
+    private void OnJump(InputValue value)
+    {
+        Debug.Log("jump");
+        if(char_characterControler.isGrounded)
+        {
+            isJumping = true;
+        }
+        
     }
 
     /// <summary>
@@ -49,11 +77,12 @@ public class PlayerMotionController : MonoBehaviour
     /// </summary>
     private void Move()
     {
-        //normalize
-
+        forwardDirection = new Vector3(0, 0, inputAxis.y * transform.forward.z);
+        rightDirection = inputAxis.x * transform.right;
+        Vector3 movement = forwardDirection + rightDirection;
+        movement.Normalize();
+        velocity = movement * walkSpeed * Time.fixedDeltaTime;     
     }
-
-
 
     /// <summary>
     /// Handles rotations
@@ -61,5 +90,26 @@ public class PlayerMotionController : MonoBehaviour
     private void Rotate()
     {
 
+    }
+
+    /// <summary>
+    /// Apply forces if the player needs to jump
+    /// </summary>
+    private void Jump()
+    {
+        if(isJumping)
+        {
+            velocity.y += Mathf.Sqrt(jumpMaxHeight * -3.0f * gravity);
+            isJumping = false;
+        }
+        velocity.y += gravity * Time.fixedDeltaTime;
+    }
+
+    /// <summary>
+    /// Apply the velocity to the character
+    /// </summary>
+    private void AddForce()
+    {
+        char_characterControler.Move(velocity);
     }
 }
