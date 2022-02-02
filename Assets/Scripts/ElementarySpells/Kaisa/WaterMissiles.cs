29ded6631c75fc19d4a3fb54ad8d47535955b2be
Candela Nicolas <darkxell.mc@gmail.com>
2022-02-02 18:39:50 +0100

    Water ball spawning algorithm
    wip, but at least looks like something.

--- Assets/Scripts/ElementarySpells/Kaisa/WaterMissiles.cs
+++ Assets/Scripts/ElementarySpells/Kaisa/WaterMissiles.cs
@@ -15,26 +15,33 @@ public class WaterMissiles : AbstractSpell
 
     private List<GameObject> balls = new List<GameObject>(20);
     private int spawnedballs = 0;
+    private bool initialized = false;
 
     public override void FixedUpdate()
     {
         timeLocale += Time.fixedDeltaTime;
-        if (spawnedballs <= BallsToSpawn) {
-            spawnedballs++;
-            balls.Add(Instantiate(BallPrefab));
+        // Spell inititalisation in scene
+        if (!initialized)
+        {
+            elementary.GetComponent<MeshRenderer>().enabled = false;
+            while (spawnedballs <= BallsToSpawn)
+            {
+                spawnedballs++;
+                balls.Add(Instantiate(BallPrefab, transform.position + randomVector(), Quaternion.identity));
+            }
         }
-
+        // Spell self destruction
         float castmax = castingTime < 0.1f ? 2 : castingTime;
-        if (timeLocale > castmax) {
+        if (timeLocale > castmax || balls.Count <= 0)
+        {
             balls.ForEach(e => { Destroy(e); });
-
             elementary.GetComponent<ElementaryController>().currentSpell = null;
             elementary.transform.position = getDestination();
             elementary.GetComponent<ElementaryController>().computePosition = true;
-
+            elementary.GetComponent<MeshRenderer>().enabled = true;
             Destroy(gameObject);
         }
-       
+
     }
 
     /// <returns>The destination position of the child missiles. </returns>
@@ -42,6 +49,11 @@ public class WaterMissiles : AbstractSpell
     {
         if (targetTransform == null) return base.target;
         return targetTransform.position;
+    }
+
+    private Vector3 randomVector()
+    {
+        return new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
     }
 
 }
