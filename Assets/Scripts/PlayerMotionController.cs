5e8dcf0d3fdc9c27f504c7c90db26a9ba3df47d5
evann delaunay <evann.delaunay@gmail.com>
2022-02-02 13:52:01 +0100

    -reworked player motion controller

--- Assets/Scripts/PlayerMotionController.cs
+++ Assets/Scripts/PlayerMotionController.cs
@@ -3,69 +3,95 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMotionController : MonoBehaviour
 {
     [Header("Character settings")]
-    [SerializeField] [Range(0, 20)] private float walkSpeed = 1f;
-    [SerializeField] [Range(0, 200)] private float rotationSpeed = 1f;
-    [SerializeField] [Range(0, 10)] private float jumpMaxHeight = 1f;
+    [Range(0, 20)] public float walkSpeed = 1f;
+    [Range(0, 10)] public float jumpForce = 1f;
     [Header("Character Physics settings")]
-    [SerializeField] [Range(0, 10)] private float dragMultipler = 1f;
-    [SerializeField] [Range(0, 10)] private float airDragMultipler = 1f;
-    [SerializeField] [Range(0, 1)] private float airControl = 1f;
-    [Header("SlopeAnglesDetection settings")]
-    [SerializeField] private float sphereCastRadius = 1;
-    [SerializeField] private float sphereCastDistance = 1;
-    [SerializeField] private float rayCastDistance = 1;
+    [Range(0, 10)] public float friction = 1f;
+    [Range(0, 10)] public float airFriction = 1f;
+    [Range(0, 1)] public float airControl = 1f;
+    [Min(0)] public float gravity = -9.81f;
+
+    [Header("SlopeAnglesDetection settings")] 
+    [SerializeField] private float groundTestRadiusFactor = 0.95f;
+    [SerializeField] private float groundMaxDistance = 0.1f;
     [SerializeField] private int layerMask;
-    [SerializeField] private float startDistanceOffset = 0.2f;
     [SerializeField] private bool debug = false;
 
 
-    public float gravity = -9.81f;
-    private Rigidbody char_body;
-    private PlayerInput char_playerInput;
-    private CharacterController char_characterControler;
-    public CameraController char_cameraController;
-    public Animator char_animator;
+    public CameraController cameraController;
+
+    private CharacterController controller;
     private Vector3 forwardDirection;
     private Vector3 rightDirection;
     private Vector2 inputAxis;
     private Vector3 velocity;
     private bool onGround;
-    
-    private bool isJumping = false;
-    // Start is called before the first frame update
+    private float floorAngle;
+    private RaycastHit surfaceInfo;
+
+
     private void Awake()
     {
-        char_characterControler = gameObject.GetComponent<CharacterController>();
-        char_animator = gameObject.GetComponent<Animator>();
-        if (char_characterControler == null)
-            Debug.LogError("PlayerMotionController : Missing characterController component on object");
-        if(gameObject.GetComponent<PlayerInput>() == null)
-            Debug.LogError("PlayerMotionController : Missing PlayerInput component on object");
+        controller = gameObject.GetComponent<CharacterController>();
     }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //forwardDirection = Input.GetAxis("Vertical");
-        //rightDirection = Input.GetAxis("Horizontal");
 
     }
 
     private void FixedUpdate()
     {
-        onGround = char_characterControler.isGrounded && GetSlopeAngle() < char_characterControler.slopeLimit;
-        Move();
-        Jump();
-        Rotate();
-        AddForce();
-        char_characterControler.Move(velocity * Time.fixedDeltaTime);
+        floorAngle = GetSlopeAngle();
+        onGround = controller.isGrounded && floorAngle < controller.slopeLimit;
+
+        #region Apply Direction Input
+
+        forwardDirection = inputAxis.y * cameraController.GetViewForward;
+        rightDirection = inputAxis.x * cameraController.GetViewRight;
+        Vector3 movement = forwardDirection + rightDirection;
+        movement.Normalize();
+        velocity += movement * walkSpeed * Time.fixedDeltaTime * (onGround ? 1 : airControl);
+
+        #endregion
+
+        #region Apply Gravity
+
+        if (!onGround)
+        {
+            if (controller.isGrounded)
+            {
+                Vector3 force = Vector3.ProjectOnPlane(Vector3.up * (gravity * Time.fixedDeltaTime), surfaceInfo.normal);
+
+                velocity += force;
+            }
+            else
+            {
+                velocity.y += gravity * Time.fixedDeltaTime;
+            }
+        }
+
+        #endregion
+
+        #region Apply Friction
+
+        Vector3 dragForce = -velocity * Time.fixedDeltaTime;
+        dragForce *= onGround ? friction : airFriction;
+        dragForce = Vector3.ClampMagnitude(dragForce, velocity.magnitude);
+        velocity += dragForce;
+
+        #endregion
+
+        controller.Move(velocity * Time.fixedDeltaTime);
     }
 
     /// <summary>
@@ -74,7 +100,6 @@ public class PlayerMotionController : MonoBehaviour
     /// <param name="value"></param>
     void OnMove(InputValue value)
     {
-        //Debug.Log(value.Get<Vector2>());
         inputAxis = value.Get<Vector2>();
         
     }
@@ -85,71 +110,12 @@ public class PlayerMotionController : MonoBehaviour
     /// <param name="value"></param>
     private void OnJump(InputValue value)
     {
-        Debug.Log("jump");
         if(onGround)
         {
-            isJumping = true;
+            onGround = false;
+            velocity.y = jumpForce;
         }
         
-    }
-
-    /// <summary>
-    /// Handles Transformations
-    /// </summary>
-    private void Move()
-    {
-        forwardDirection = inputAxis.y * char_cameraController.GetViewForward;
-        rightDirection = inputAxis.x * char_cameraController.GetViewRight;
-        Vector3 movement = forwardDirection + rightDirection;
-        movement.Normalize();
-        velocity += movement * walkSpeed * Time.fixedDeltaTime * (onGround ? 1 : airControl);     
-    }
-
-    /// <summary>
-    /// Handles rotations
-    /// </summary>
-    private void Rotate()
-    {
-        Vector3 cameraDirection = char_cameraController.GetViewDirection;
-    }
-
-    /// <summary>
-    /// Apply forces if the player needs to jump
-    /// </summary>
-    private void Jump()
-    {
-        if(isJumping)
-        {
-            velocity.y = Mathf.Sqrt(jumpMaxHeight * -3.0f * gravity);
-            isJumping = false;
-        }
-        
-    }
-
-    /// <summary>
-    /// Apply forces to the character
-    /// </summary>
-    private void AddForce()
-    {
-        
-        if (!onGround)
-        {
-            velocity.y += gravity * Time.fixedDeltaTime;
-        }
-        velocity += DragForce();
-    }
-
-    /// <summary>
-    /// Calculate drag force for the current movement
-    /// </summary>
-    /// <returns></returns>
-    private Vector3 DragForce()
-    {
-        Vector3 force = -velocity;
-        force *= Time.fixedDeltaTime;
-        force *= onGround ? dragMultipler : airDragMultipler;
-        force = Vector3.ClampMagnitude(force, velocity.magnitude);
-        return force;
     }
 
     /// <summary>
@@ -157,18 +123,11 @@ public class PlayerMotionController : MonoBehaviour
     /// </summary>
     private float GetSlopeAngle()
     {
-        RaycastHit hit;
-        float angleOneInDegrees = Mathf.Epsilon;
-        float angleTwoInDegrees = Mathf.Epsilon;// FOR LATER (Edges & corners)
-        Vector3 SphereCastOrigin = new Vector3(transform.position.x, transform.position.y - (char_characterControler.height / 2) + startDistanceOffset, transform.position.z);
-        if (Physics.SphereCast(SphereCastOrigin, sphereCastRadius, Vector3.down, out hit, sphereCastDistance, layerMask))
+        float angleOneInDegrees = 0;
+        float angleTwoInDegrees = 0;// FOR LATER (Edges & corners)
+        if (Physics.SphereCast(transform.position, controller.radius* groundTestRadiusFactor, Vector3.down, out surfaceInfo, controller.height/2-controller.radius + groundMaxDistance, layerMask))
         {
-            angleOneInDegrees = Vector3.Angle(hit.normal, Vector3.up);
-            if(angleOneInDegrees > char_characterControler.slopeLimit && debug)
-            {
-                
-                Debug.Log(angleOneInDegrees);
-            }
+            angleOneInDegrees = Vector3.Angle(surfaceInfo.normal, Vector3.up);
         }
 
         return angleOneInDegrees;
@@ -177,21 +136,17 @@ public class PlayerMotionController : MonoBehaviour
 
     void OnDrawGizmosSelected()
     {
-        if (debug)
+        if (debug && Application.isPlaying)
         {
-            // Visualize SphereCast with two spheres and a line
-            Vector3 startPoint = new Vector3(transform.position.x, transform.position.y - (char_characterControler.height / 2) + startDistanceOffset, transform.position.z);
-            Vector3 endPoint = new Vector3(transform.position.x, transform.position.y - (char_characterControler.height / 2) + startDistanceOffset - sphereCastDistance, transform.position.z);
-            if(startPoint != null && endPoint != null)
-            {
-                Gizmos.color = Color.white;
-                Gizmos.DrawWireSphere(startPoint, sphereCastRadius);
+            Vector3 end = transform.position + Vector3.down * (controller.height / 2 + groundMaxDistance - controller.radius);
 
-                Gizmos.color = Color.gray;
-                Gizmos.DrawWireSphere(endPoint, sphereCastRadius);
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireSphere(transform.position, controller.radius * groundTestRadiusFactor);
 
-                Gizmos.DrawLine(startPoint, endPoint);
-            }
+            Gizmos.color = Color.gray;
+            Gizmos.DrawWireSphere(end, controller.radius * groundTestRadiusFactor);
+
+            Gizmos.DrawLine(transform.position, end);
             
         }
     }
