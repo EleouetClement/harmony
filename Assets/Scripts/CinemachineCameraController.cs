31249bd0b0551941137f8ea3971eb06194851977
Constantin <constantin.mgn@gmail.com>
2022-02-10 11:04:21 +0100

    Shot test 1

---
+++ Assets/Scripts/CinemachineCameraController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CinemachineCameraController : MonoBehaviour
+{
+    public Transform PlayerMesh;
+    [Range(0, 90)] public float verticalMaxAngle = 85.0f;
+    [Min(0)] public float sensibility = 0.25f;
+
+    private Vector3 targetPosition;
+    private Vector2 lookInput;
+    private Vector2 rotation;
+
+    static private Vector3 groundVector = new Vector3(0, 1, 0);
+
+    public Vector3 GetViewDirection
+    {
+        get
+        {
+            return transform.forward;
+        }
+    }
+
+    public Vector3 GetViewForward
+    {
+        get
+        {
+            return Vector3.ProjectOnPlane(transform.forward, Vector3.up);
+        }
+    }
+
+    public Vector3 GetViewRight
+    {
+        get
+        {
+            return transform.right;
+        }
+    }
+
+    private void Update()
+    {
+        //PlayerMesh.rotation = Quaternion.Euler(0, rotation.y, 0);
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        AddYaw(lookInput.x * Time.deltaTime);
+        AddPitch(-lookInput.y * Time.deltaTime);
+
+        Quaternion camRotation = Quaternion.Euler(rotation.x, rotation.y, 0);
+        transform.localRotation = camRotation;
+        
+    }
+
+    public void AddYaw(float yaw)
+    {
+        rotation.y += yaw * sensibility;
+
+        if (rotation.y < 0f)
+            rotation.y += 360f;
+        else if (rotation.y >= 360f)
+            rotation.y -= 360f;
+    }
+
+    public void AddPitch(float pitch)
+    {
+        rotation.x += pitch * sensibility;
+        rotation.x = Mathf.Clamp(rotation.x, -verticalMaxAngle, verticalMaxAngle);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        
+        lookInput = value.Get<Vector2>() * 100;
+    }
+
+
+}
