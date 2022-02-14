5643a54a5bd1bf8d16144572a5b76a85166fd127
Iwan N <iwan.natij@gmail.com>
2022-02-14 10:40:02 +0100

    Earth spell tweaks

--- Assets/Scripts/ElementarySpells/Earth/EarthMortar.cs
+++ Assets/Scripts/ElementarySpells/Earth/EarthMortar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EarthMortar : AbstractSpell
 {
@@ -10,14 +11,15 @@ public class EarthMortar : AbstractSpell
 	public Vector3 lastBallCoord;
 	[Min(1)] public float maxSpellLength;
 	public float maxRange;
-	
 
+	GameObject hud;
 	
 
 	private void Start()
 	{
 		maxCastTime = 1.5f;
-		
+		blinkTiming = 1f;
+		hud = GameObject.Find("GameManager").GetComponent<GameModeSingleton>().GetPlayerHUD;
 	}
 
 	public override void init(GameObject elemRef, Vector3 target)
@@ -35,24 +37,37 @@ public class EarthMortar : AbstractSpell
 	public override void FixedUpdate()
 	{
 		base.FixedUpdate();
+
 		if (ball == null)
 		{
 			elementary.transform.position = lastBallCoord;
 			elementary.GetComponent<MeshRenderer>().enabled = true;
 			elementary.GetComponent<ElementaryController>().currentSpell = null;
 			elementary.GetComponent<ElementaryController>().computePosition = true;
+			hud.transform.Find("Charge bar").gameObject.GetComponent<Slider>().enabled = false;
 			Destroy(gameObject);
 		}
 		else 
 		{
 			//refreshes charge level to earth ball
 			ball.GetComponent<EarthBall>().charge = charge / maxCastTime;
+			hud.transform.Find("Charge bar").gameObject.GetComponent<Slider>().enabled = true;
+			print("non");
+			hud.transform.Find("Charge bar").gameObject.GetComponent<Slider>().value = charge / maxCastTime;
 		}
+
+		
+			
 
 	}
 	protected override void onChargeEnd(float chargetime)
 	{
 		earthMarker.DestroyMarker();
+
+		if (chargetime >= 1.05f || chargetime <= 9.95f)
+			print("Blink!");
+		Debug.Log("yes");
+		//hud.transform.Find("Charge bar").gameObject.GetComponent<Slider>().enabled = false;
 		ball.GetComponent<EarthBall>().Launch();
 	}
 
