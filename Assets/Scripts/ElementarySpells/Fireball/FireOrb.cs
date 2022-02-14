42ccce9559c1d75730ee64be2ecd72b70153083e
EleouetClement <eleouet.clement@outlook.fr>
2022-02-14 13:04:29 +0100

    Added some features for a fire effect when fireball hits something

--- Assets/Scripts/ElementarySpells/Fireball/FireOrb.cs
+++ Assets/Scripts/ElementarySpells/Fireball/FireOrb.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class FireOrb : MonoBehaviour
 {
+    [SerializeField] private GameObject firePrefab;
+    private bool blinked = false;
+    private bool environment = false;
+    private bool hasExplode = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +23,34 @@ public class FireOrb : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("pouet");
+        if(!environment && !hasExplode)
+        {
+            Explode();
+        }       
     }
+
+    /// <summary>
+    /// Activate blinked spell stats
+    /// </summary>
+    public void SetBlinkBehaviour()
+    {
+        blinked = true;
+    }
+
+    public void IsEnvironmentSpell(bool environment)
+    {
+        this.environment = environment;
+    }
+    private void Explode()
+    {
+        Debug.Log("Explosion");
+        Instantiate(firePrefab, transform.position, Quaternion.identity);
+        hasExplode = true;
+    }
+
+    private void BlinkBehaviour()
+    {
+        //TO DO...
+    }
+
 }
