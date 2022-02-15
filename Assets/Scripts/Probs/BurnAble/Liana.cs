ea9675c9b0e45526e92e19913d4d1afaf1a114db
EleouetClement <eleouet.clement@outlook.fr>
2022-02-15 14:27:14 +0100

    Added lianas prefab + liana burn behaviour

--- Assets/Scripts/Probs/BurnAble/Liana.cs
+++ Assets/Scripts/Probs/BurnAble/Liana.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public sealed class Liana : BurnableItem
 {
+    [SerializeField] [Min(0)] private float transformSpeed;
+    [SerializeField] [Min(0)] private float scaleReductionSpeed;
     protected override void Update()
     {
         if (triggered)
         {
-            
-        }
+            if(transform.localScale.y > Mathf.Epsilon)
+            {
+                Burn();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }     
     }
 
     public override void Consume()
     {
         base.Consume();
         Debug.Log("Lianas on fire!");
+    }
+
+    private void Burn()
+    {
+        transform.Translate(Vector3.up * transformSpeed * Time.deltaTime);
+        Vector3 newScale = Vector3.down * scaleReductionSpeed * Time.deltaTime;
+        transform.localScale += newScale;
     }
 }
