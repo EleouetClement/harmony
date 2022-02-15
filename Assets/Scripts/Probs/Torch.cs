b67e74002e1ce8e2badf4be8782e98edac2ba3af
EleouetClement <eleouet.clement@outlook.fr>
2022-02-15 12:27:21 +0100

    Updated prefab to have only one Go for a torch

--- Assets/Scripts/Probs/Torch.cs
+++ Assets/Scripts/Probs/Torch.cs
@@ -4,28 +4,14 @@ using UnityEngine;
 
 public class Torch : MonoBehaviour
 {
-
-    [SerializeField] GameObject fire;
-
     private bool triggered = false;
     private ParticleSystem fireSystem;
 
     // Start is called before the first frame update
     private void Awake()
     {
-        fireSystem = fire.GetComponent<ParticleSystem>();
+        fireSystem = GetComponent<ParticleSystem>();
         fireSystem.Stop();
-    }
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     private void OnCollisionEnter(Collision collision)
