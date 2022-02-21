b26eb171c1b1eae827bcf1f27d620b7f026b5812
Yvan <yvankiller92@gmail.com>
2022-02-21 15:39:32 +0100

    Add particle effects at water beam impact

--- Assets/Scripts/ElementarySpells/Water/WaterBeam.cs
+++ Assets/Scripts/ElementarySpells/Water/WaterBeam.cs
@@ -9,8 +9,8 @@ public class WaterBeam : AbstractSpell
 
     public GameObject PosMarkerPrefab;
     public CinemachineCameraController cameraController;
-
     public ElementaryController elemCtrl;
+    public ParticleSystem impactEffect;
     private RaycastHit hit;
 
     public void LateUpdate()
@@ -29,6 +29,9 @@ public class WaterBeam : AbstractSpell
                 hit.point = virtualPointDistanceMax;
             }
 
+            //impactEffect.transform.rotation = Quaternion.LookRotation()
+            impactEffect.transform.position = hit.point;
+            impactEffect.transform.LookAt(elemCtrl.transform.position);
             transform.LookAt(hit.point);
         }
     }
@@ -42,6 +45,7 @@ public class WaterBeam : AbstractSpell
         elemCtrl = elemRef.GetComponent<ElementaryController>();
         cameraController = GameModeSingleton.GetInstance().GetCinemachineCameraController;
         marker.Init(maxDistance, PosMarkerPrefab);
+        impactEffect.Play();
     }
 
     public override void Terminate()
@@ -55,6 +59,7 @@ public class WaterBeam : AbstractSpell
     protected override void onChargeEnd(float chargetime)
     {
         Destroy(marker.gameObject);
+        impactEffect.Stop();
         Terminate();
     }
 }
