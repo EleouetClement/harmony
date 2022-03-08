146038b369891931c3a0fe646f965a20561e5962
Iwan N <iwan.natij@gmail.com>
2022-03-08 12:22:00 +0100

    Merge fix + started fix

--- Assets/Scripts/PlayerGameplayController.cs
+++ Assets/Scripts/PlayerGameplayController.cs
@@ -79,7 +79,7 @@ public class PlayerGameplayController : MonoBehaviour, IDamageable
             manaburnout = true;
             if(elementaryController.currentSpell)
             {
-                elementaryController.currentSpell.Terminate();
+                elementaryController.currentSpell.OnRelease();
             }
         }
         if (manaburnout)
@@ -143,7 +143,7 @@ public class PlayerGameplayController : MonoBehaviour, IDamageable
             elementaryController.SetElement(AbstractSpell.Element.Earth);
             elementaryController.transform.GetChild(0).gameObject.GetComponent<Light>().color = Color.yellow;
         }
-        print("Element sélectionné : " + elementaryController.currentElement);
+        //print("Element sélectionné : " + elementaryController.currentElement);
     }
     #region Spell casting
 
