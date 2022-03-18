e9f276cc4e7ba839020c9ecad411dc0cda70c04c
Yvan <yvankiller92@gmail.com>
2022-03-18 17:53:58 +0100

    Bug fix movable object interactions (WIP)

--- Assets/Scripts/Probs/MovableObject.cs
+++ Assets/Scripts/Probs/MovableObject.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class MovableObject : MonoBehaviour, IDamageable
 {
     public float movingSpeed;
+    //public float forceToApply;
+
+    //private Rigidbody rigidBody;
+
+    private void Start()
+    {
+        //rigidBody = GetComponent<Rigidbody>();
+    }
 
     public void OnDamage(DamageHit hit)
     {
         float move = movingSpeed * Time.deltaTime;
         transform.position = Vector3.Lerp(transform.position, transform.position + hit.direction, move);
+        //rigidBody.AddForce(hit.direction * forceToApply);
     }
 }
