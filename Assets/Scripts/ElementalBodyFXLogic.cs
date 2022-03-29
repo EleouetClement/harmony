d45c6fa82dc501dcea273d91fb2237173fb6d2b0
Candela Nicolas <darkxell.mc@gmail.com>
2022-03-29 11:36:29 +0200

    Added ele breathe animation

---
+++ Assets/Scripts/ElementalBodyFXLogic.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementalBodyFXLogic : MonoBehaviour
+{
+
+    private Vector3 StartPosRefLocale;
+    private float timeLocale = 0f;
+
+    public float BreatheAmp = 0.1f;
+    public float BreathTime = 4f;
+
+    public float FrontAmp = 0.05f;
+    public float FrontTime = 10f;
+
+    void Start()
+    {
+        StartPosRefLocale = transform.localPosition;
+    }
+
+    void Update()
+    {
+        timeLocale += Time.deltaTime;
+
+        float ratiobreathe = (timeLocale % BreathTime) / BreathTime;
+        float newy = StartPosRefLocale.y + Mathf.Sin(ratiobreathe * Mathf.PI * 2) * BreatheAmp;
+
+        float ratiofront = (timeLocale % FrontTime) / FrontTime;
+        float newz = StartPosRefLocale.z + Mathf.Sin(ratiofront * Mathf.PI * 2) * FrontAmp;
+
+        transform.localPosition = new Vector3(StartPosRefLocale.x, newy, newz);
+    }
+}
