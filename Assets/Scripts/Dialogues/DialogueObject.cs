15056bfb47ed2a1bc3e17c681e2e56592e9f3b1c
Yvan <yvankiller92@gmail.com>
2022-04-01 17:25:58 +0200

    Update dialogue system

--- Assets/Scripts/Dialogues/DialogueObject.cs
+++ Assets/Scripts/Dialogues/DialogueObject.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Dialogue/DialogueObject")]
 public class DialogueObject : ScriptableObject
 {
+    [SerializeField] private string characterName;
     [SerializeField] [TextArea] private string[] dialogue;
 
     public string[] Dialogue => dialogue;
+    public string CharacterName => characterName;
 }
--- Assets/Scripts/Dialogues/DialogueUI.cs
+++ Assets/Scripts/Dialogues/DialogueUI.cs
@@ -5,9 +5,12 @@ using TMPro;
 public class DialogueUI : MonoBehaviour
 {
     [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private TMP_Text nameLabel;
     [SerializeField] private TMP_Text textLabel;
     [SerializeField] private DialogueObject testDialogue;
+    [SerializeField] private DialogueObject[] dialogueObjectList;
 
+    private int currentIndexDialogue = 0;
     private TypeWriterEffect typeWriterEffect;
 
     private void Start()
@@ -17,10 +20,32 @@ public class DialogueUI : MonoBehaviour
         //ShowDialogue(testDialogue);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            //ShowDialogue(testDialogue);
+            if (currentIndexDialogue < dialogueObjectList.Length)
+            {
+                ShowDialogue(dialogueObjectList[currentIndexDialogue]);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("TESTTTTTTTTT");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            CloseDialogueBox();
+        }
+    }
+
     public void ShowDialogue(DialogueObject dialogueObject)
     {
+        nameLabel.text = dialogueObject.CharacterName;
         dialogueBox.SetActive(true);
         StartCoroutine(StepThroughDialogue(dialogueObject));
+        currentIndexDialogue++;
     }
 
     private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
@@ -34,7 +59,7 @@ public class DialogueUI : MonoBehaviour
             textLabel.text = dialogue;
 
             yield return null;
-            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
         }
 
         CloseDialogueBox();
@@ -48,7 +73,7 @@ public class DialogueUI : MonoBehaviour
         {
             yield return null;
 
-            if(Input.GetKeyDown(KeyCode.Space))
+            if(Input.GetKeyDown(KeyCode.Return))
             {
                 typeWriterEffect.Stop();
             }
@@ -58,6 +83,7 @@ public class DialogueUI : MonoBehaviour
     public void CloseDialogueBox()
     {
         dialogueBox.SetActive(false);
+        nameLabel.text = string.Empty;
         textLabel.text = string.Empty;
     }
 }
