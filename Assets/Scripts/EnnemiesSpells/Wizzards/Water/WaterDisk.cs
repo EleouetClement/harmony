601232f3194131372166f8a3616ca3f30f1abb8d
EleouetClement <eleouet.clement@outlook.fr>
2022-04-04 15:25:46 +0200

    Some set ups for the disk
    trying to apply some curved trajectory for the disk for some effects

--- Assets/Scripts/EnnemiesSpells/Wizzards/Water/WaterDisk.cs
+++ Assets/Scripts/EnnemiesSpells/Wizzards/Water/WaterDisk.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class WaterDisk : EnnemySpell
 {
-    
+    [Header("Casting stats")]
+    [SerializeField] [Min(0)] int damagesGrowthPerSec;
+    [SerializeField] [Min(0)] int bounsNumber;
+    [Header("behaviour stats")]
+    [SerializeField] [Range(1, 4)] int baseDamages;
+    [SerializeField] float speed;
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("Do not change")]
+    [SerializeField] DiskManager waterDiskReference;
+    [SerializeField] bool debug = false;
+
+    private DiskManager waterDiskInstance;
+
+    private Vector3 trajectory = Vector3.zero;
+
+    private void Awake()
     {
-        
+        if (!waterDiskReference)
+        {
+            Debug.LogError("Ennemy water spell : no water disk reference, spell cannot launch");
+            Destroy(gameObject);
+        }
+
     }
 
     // Update is called once per frame
@@ -18,6 +35,28 @@ public class WaterDisk : EnnemySpell
         
     }
 
+    public override void Charge(CastType chargeTime, Transform spellOrigin)
+    {
+        base.Charge(chargeTime, spellOrigin);
+        waterDiskInstance = Instantiate(waterDiskReference, summonerPosition.position, Quaternion.identity);
+        damagesDeal = new DamageHit(baseDamages, AbstractSpell.Element.Water);
+    }
+
+    public override void Charge(CastType chargeTime, Transform spellOrigin, Vector3 targetPosition)
+    {
+        base.Charge(chargeTime, spellOrigin, targetPosition);
+        waterDiskInstance = Instantiate(waterDiskReference, summonerPosition.position, Quaternion.identity);
+        damagesDeal = new DamageHit(baseDamages, AbstractSpell.Element.Water);
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        if(!charged)
+        {
+            //Nothing to ad yet for the casting
+        }
+    }
 
     public override void Terminate()
     {
@@ -27,5 +66,16 @@ public class WaterDisk : EnnemySpell
     protected override void OnChargeEnd()
     {
         throw new System.NotImplementedException();
+        //Trajectory calculation to do
+    }
+
+    private void Fly()
+    {
+
+    }
+
+    protected override void DealDamages(GameObject objectHitted)
+    {
+        base.DealDamages(objectHitted);
     }
 }
