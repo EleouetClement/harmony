c8df6de7f12b3e21fbe3457247d3bcebcb7dd38d
EleouetClement <eleouet.clement@outlook.fr>
2022-04-04 14:36:29 +0200

    Created scripts for waterDisk

---
+++ Assets/Scripts/EnnemiesSpells/Wizzards/Water/WaterDisk.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDisk : EnnemySpell
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public override void Terminate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnChargeEnd()
+    {
+        throw new System.NotImplementedException();
+    }
+}
