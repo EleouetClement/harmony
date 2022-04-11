6ee24d5dcb63afe6756ba2f2f88acd341ad17b21
Iwan N <iwan.natij@gmail.com>
2022-04-11 17:37:54 +0200

    Controls settings

---
+++ Assets/Scripts/UI/Settings/CameraSlider.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class CameraSlider : MonoBehaviour
+{
+    Slider slider;
+    CinemachineCameraController cameraController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        cameraController = GameModeSingleton.GetInstance().GetCinemachineCameraController;
+        var range = typeof(CinemachineCameraController).GetField(nameof(CinemachineCameraController.sensibility)).GetCustomAttribute<RangeAttribute>();
+        slider.maxValue = range.max;
+        slider.minValue = range.min;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.onValueChanged.AddListener(ChangeSensitivity);
+    }
+
+    void ChangeSensitivity(float val)
+    {
+        cameraController.sensibility = val;
+    }
+}
