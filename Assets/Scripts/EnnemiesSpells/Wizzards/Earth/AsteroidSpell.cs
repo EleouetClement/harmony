041da9bab768a14ac4020dd987b53c87e3251541
celeouet <eleouet.clement@outlook.fr>
2022-04-11 10:25:22 +0200

    Trying to create same earth spell as the player's spell

--- Assets/Scripts/EnnemiesSpells/Wizzards/Earth/AsteroidSpell.cs
+++ Assets/Scripts/EnnemiesSpells/Wizzards/Earth/AsteroidSpell.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AsteroidSpell : MonoBehaviour
+public class AsteroidSpell : EnnemySpell
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Casting stats")]
+    [SerializeField] [Range(0, 0.1f)] float projectileAdditionnalScale;
+    [SerializeField] [Min(0)] int damagesGrowthPerSec;
+    private float projectileGrowthPerSec;
+
+    //should be counted in number of hits according to the player health system
+    [Header("behaviour stats")]
+    [SerializeField] [Range(1, 4)] int baseDamages;
+    [SerializeField] float speed;
+
+
+    [Header("Do not change")]
+    [SerializeField] EnnemiFireBall rockReference;
+    [SerializeField] bool debug = false;
+    [SerializeField] float explosionradius;
+    [SerializeField] LayerMask blastEffect;
+    [SerializeField] LayerMask blastCheck;
+
+    private AsteroidController rockInstance;
+
+    private Vector3 trajectory = Vector3.zero;
+
+    private void Awake()
     {
-        
+        if (!rockReference)
+        {
+            Debug.LogError("Ennemy earth Spell : no rock reference, spell cannot launch");
+            Destroy(gameObject);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Terminate()
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnChargeEnd()
+    {
+        throw new System.NotImplementedException();
     }
 }
