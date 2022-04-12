e5dfbd44bcfa52e2d3b03758c722ce5d0af73d5e
Evann Delaunay <evann.delaunay@gmail.com>
2022-04-12 15:01:36 +0200

    added warrior weapon

--- Assets/Scripts/AI/WariorAI.cs
+++ Assets/Scripts/AI/WariorAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Harmony.AI;
@@ -5,7 +6,17 @@ using UnityEngine;
 
 public class WariorAI : AIAgent
 {
+    public DamageBox swordBox;
+
+    private bool hasHit = false;
     private static readonly int Speed = Animator.StringToHash("Speed");
+
+    private void Start()
+    {
+        swordBox.OnDamage.AddListener(OnDamagePlayer);
+        swordBox.OnShielded.AddListener(OnShielded);
+        swordBox.OnPerfectShielded.AddListener(OnPerfectShielded);
+    }
 
     protected override void Update()
     {
@@ -16,10 +27,36 @@ public class WariorAI : AIAgent
     public void Attack()
     {
         animator.SetTrigger("Attack");
+        swordBox.active = true;
+        hasHit = false;
     }
 
     public void FinishedAttack()
     {
-        treeRunner.tree.blackboard.currentState = "Chase"; 
+        if (hasHit)
+        {
+            hasHit = false;
+            treeRunner.tree.blackboard.currentState = "Chase";
+        }
+        else
+        {
+            treeRunner.tree.blackboard.currentState = "Chase";
+        }
+    }
+
+    private void OnDamagePlayer()
+    {
+        hasHit = true;
+    }
+
+    private void OnShielded()
+    {
+        animator.SetTrigger("Impact");
+    }
+
+    private void OnPerfectShielded()
+    {
+        OnDamage(new DamageHit(100000));
+        animator.SetTrigger("Impact");
     }
 }
