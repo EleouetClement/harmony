e7f62c5ab53567b4654235b635db8d0d1ff2bcef
Yvan <yvankiller92@gmail.com>
2022-04-13 11:01:04 +0200

    Udpate checkpoint & death zone system

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -1,26 +1,66 @@
+using System.Collections;
 using UnityEngine;
 
 public class DeathZone : MonoBehaviour
 {
+    public CanvasGroup fadeScreen;
+    public float fadeInTime = 3f;
+    public float fadeOutTime = 1f;
+
     private Transform playerSpawn;
-    public GameObject player;
+    private GameObject player;
+
+    private float elapsedTime = 0f;
+    private bool isFadingIn = false;
+    private bool isFadingOut = false;
 
     private void Awake()
     {
         playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
+        player = GameModeSingleton.GetInstance().GetPlayerReference;
     }
 
     private void OnTriggerEnter(Collider collider)
     {
         if(collider.gameObject.layer == HarmonyLayers.LAYER_PLAYER)
         {
-            Debug.Log("TRIGGERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR");
-            //GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
-            //GameModeSingleton.GetInstance().GetPlayerReference.transform.position = playerSpawn.position;
-            //GameObject.FindGameObjectWithTag("Player").transform.position = playerSpawn.position;
-            //collider.transform.position = playerSpawn.position;
-            //player.transform.position = playerSpawn.position;
-            //collider.transform.Translate(playerSpawn.position);
+            fadeScreen.alpha = 0f;
+            fadeScreen.gameObject.SetActive(true);
+            isFadingIn = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isFadingIn)
+        {
+            elapsedTime += Time.deltaTime;
+            elapsedTime = Mathf.Clamp(elapsedTime, 0f, fadeInTime);
+            fadeScreen.alpha = 0f + (elapsedTime / fadeInTime);
+
+            if (fadeScreen.alpha >= 1f)
+            {
+                player.SetActive(false);
+                player.transform.position = playerSpawn.position;
+                player.SetActive(true);
+
+                isFadingIn = false;
+                isFadingOut = true;
+                elapsedTime = 0f;
+            }
+        }
+
+        if (isFadingOut)
+        {
+            elapsedTime += Time.deltaTime;
+            elapsedTime = Mathf.Clamp(elapsedTime, 0f, fadeOutTime);
+            fadeScreen.alpha = 1f - (elapsedTime / fadeOutTime);
+
+            if (fadeScreen.alpha <= 0)
+            {
+                isFadingOut = false;
+                elapsedTime = 0f;
+            }
         }
     }
 }
