6c381b3e4c35a912b81914f22a84fbafd785f929
Yvan <yvankiller92@gmail.com>
2022-04-13 11:48:27 +0200

    Last update on player death

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -15,6 +15,7 @@ public class CheckPoint : MonoBehaviour
     {
         if(collider.gameObject.layer == HarmonyLayers.LAYER_PLAYER)
         {
+            // Change the position of the player spawn to the position of this checkpoint
             playerSpawn.position = transform.position;
             Destroy(gameObject);
         }
--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DeathZone : MonoBehaviour
 {
     public CanvasGroup fadeScreen;
-    public float fadeInTime = 3f;
+    public float fadeInTime = 1f;
     public float fadeOutTime = 1f;
 
     private Transform playerSpawn;
     private GameObject player;
+    private PlayerInput playerInput;
 
-    private float elapsedTime = 0f;
+    private float elapsedTime = 0f; // current time during the fade
     private bool isFadingIn = false;
     private bool isFadingOut = false;
 
@@ -18,6 +20,7 @@ public class DeathZone : MonoBehaviour
     {
         playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
         player = GameModeSingleton.GetInstance().GetPlayerReference;
+        playerInput = player.GetComponent<PlayerInput>();
     }
 
     private void OnTriggerEnter(Collider collider)
@@ -27,6 +30,7 @@ public class DeathZone : MonoBehaviour
             fadeScreen.alpha = 0f;
             fadeScreen.gameObject.SetActive(true);
             isFadingIn = true;
+            playerInput.DeactivateInput(); // Stop player's movements
         }
     }
 
@@ -34,15 +38,18 @@ public class DeathZone : MonoBehaviour
     {
         if (isFadingIn)
         {
+            // Fade in
             elapsedTime += Time.deltaTime;
             elapsedTime = Mathf.Clamp(elapsedTime, 0f, fadeInTime);
             fadeScreen.alpha = 0f + (elapsedTime / fadeInTime);
 
             if (fadeScreen.alpha >= 1f)
             {
+                // Replace the player to the spawn position
                 player.SetActive(false);
                 player.transform.position = playerSpawn.position;
                 player.SetActive(true);
+                playerInput.DeactivateInput(); // Stop player's movements
 
                 isFadingIn = false;
                 isFadingOut = true;
@@ -52,12 +59,14 @@ public class DeathZone : MonoBehaviour
 
         if (isFadingOut)
         {
+            // Fade out
             elapsedTime += Time.deltaTime;
             elapsedTime = Mathf.Clamp(elapsedTime, 0f, fadeOutTime);
             fadeScreen.alpha = 1f - (elapsedTime / fadeOutTime);
 
             if (fadeScreen.alpha <= 0)
             {
+                playerInput.ActivateInput(); // Allow player's movements
                 isFadingOut = false;
                 elapsedTime = 0f;
             }
