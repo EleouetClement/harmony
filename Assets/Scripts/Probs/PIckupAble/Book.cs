4211b017d0cc2883e4793df58ca133cd226d85bd
EleouetClement <eleouet.clement@outlook.fr>
2022-04-13 10:53:51 +0200

    Created book mecanic

--- Assets/Scripts/Probs/PIckupAble/Book.cs
+++ Assets/Scripts/Probs/PIckupAble/Book.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Book : MonoBehaviour
 {
-
+    public enum PageType
+    {
+        Journal,
+        Tips,
+    }
     [SerializeField] [Min(0)] int index = 0;
+    [SerializeField] PageType type;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +26,22 @@ public class Book : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("PouetLivre");
+        Debug.Log("Livre d'index " + index);
+        InventoryManager inv = InventoryManager.instance;
+        if(type.Equals(PageType.Journal))
+        {
+            inv.UnlockJournalInInventory(index);
+        }
+        else
+        {
+            inv.UnlockTipInInventory(index);
+        }
+        PlayPickUpSound();
+    }
+
+    private void PlayPickUpSound()
+    {
+        //Activate pickup sound before destroying item
+        //TO DO...
     }
 }
